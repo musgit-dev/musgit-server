020c80b08121a116f3e3f9129a9a0efb9a757bbf
Andrei Markin <andrey.markin.ppc@gmail.com>
2025-07-23 22:54:44 +0400

    feat: add routes for interacting with lessons
    * Rename library to github.com/musgit-dev/musgit-server

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
-	"musgit"
 	"net/http"
+
+	"github.com/musgit-dev/musgit"
 
 	"github.com/gin-gonic/gin"
 )
@@ -11,10 +14,37 @@ import (
 func main() {
 	router := gin.Default()
 
-	service := musgit.MusgitService()
+	dbUri := flag.String("db-uri", "", "Data with data")
+	flag.Parse()
 
-	router.GET("/pieces/", func(c *gin.Context) {
-		res := []string{"foo", "bar"}
+	if *dbUri == "" {
+		log.Fatal("DB not provided.")
+	}
+	musgitService := musgit.NewMusgitService(*dbUri)
+
+	router.GET("/lessons/", func(c *gin.Context) {
+		res := musgitService.GetLessons()
+		c.JSON(http.StatusOK, res)
+	})
+
+	router.POST("/lessons/", func(c *gin.Context) {
+		res, err := musgitService.StartLesson()
+		if err != nil {
+			log.Fatal(err)
+		}
+		c.JSON(http.StatusOK, fmt.Sprintf("Started new lesson: %d", res.ID))
+	})
+
+	router.POST("/lessons/stop", func(c *gin.Context) {
+		res := musgitService.StopCurrentLesson()
+		c.JSON(http.StatusOK, res)
+	})
+	router.POST("/lessons/pause", func(c *gin.Context) {
+		res := musgitService.PauseCurrentLesson()
+		c.JSON(http.StatusOK, res)
+	})
+	router.POST("/lessons/resume", func(c *gin.Context) {
+		res := musgitService.ResumeCurrentLesson()
 		c.JSON(http.StatusOK, res)
 	})
 	if err := router.Run(); err != nil {
