1f5cd72d6f60ade7ec4760101f714bead0ded8ce
Andrei Markin <andrey.markin.ppc@gmail.com>
2026-03-26 12:14:13 +0400

    feat: add otel traces and instrument gin

--- routes/main.go
+++ routes/main.go
@@ -1,7 +1,11 @@
 package routes
 
 import (
+	"context"
 	"log"
+
+	"go.opentelemetry.io/contrib/instrumentation/github.com/gin-gonic/gin/otelgin"
+	"go.opentelemetry.io/otel/trace"
 
 	"github.com/musgit-dev/musgit"
 
@@ -10,15 +14,25 @@ import (
 
 var router = gin.Default()
 
-func Run(mg *musgit.Musgit) {
-	getRoutes(mg)
+func Run(
+	ctx context.Context,
+	mg *musgit.Musgit, tracer trace.Tracer) {
+
+	getRoutes(ctx, mg, tracer)
 	if err := router.Run(); err != nil {
 		log.Fatal(err)
 	}
 }
 
-func getRoutes(mg *musgit.Musgit) {
+func getRoutes(
+	ctx context.Context,
+	mg *musgit.Musgit,
+	tracer trace.Tracer,
+) {
+	ctx, span := tracer.Start(ctx, "get-routes")
+	defer span.End()
 	api := router.Group("/api")
+	api.Use(otelgin.Middleware("musgit-service"))
 	lessonRoutes(mg, api)
 	pieceRoutes(mg, api)
 }
